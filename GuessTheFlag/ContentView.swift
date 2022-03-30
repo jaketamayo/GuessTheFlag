d9dc3c77d81f7c851ba76d06209371fd3e970e16
Jake Tamayo <hellojtmatthew@icloud.com>
2022-03-30 12:19:00 -0500

    add function for correct score and show alert after flag is tapped

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    //1. Create an array of country names
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    //Show an alert to keep track of user progess
+    @State private var showingScore = false
+    
+    //Property to store message title in the alert
+    @State private var scoreTitle = ""
+    
+    // Create an array of country names
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
     //2. Display 3 flags (1-3) that represet the correct awnser - flags are random
-    var correctAnwser = Int.random(in: 0...2)
+    @State private var correctAnwser = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -27,7 +34,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { number in
                     Button {
-                        //flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -35,6 +42,24 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    func flagTapped(_ number: Int){
+        if number == correctAnwser {
+            scoreTitle = "Correct"
+        }else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffled()
+        correctAnwser = Int.random(in: 0...2)
     }
 }
 
