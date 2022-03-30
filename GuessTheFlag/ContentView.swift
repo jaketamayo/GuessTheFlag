bec8249081151c581f81fb39f54acf83c4d90a30
Jake Tamayo <hellojtmatthew@icloud.com>
2022-03-30 11:41:11 -0500

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jacob Tamayo on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //1. Create an array of country names
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    //2. Display 3 flags (1-3) that represet the correct awnser - flags are random
+    var correctAnwser = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.purple
+                .ignoresSafeArea()
+            VStack(spacing: 20){
+                VStack {
+                    Text("Choose the correct flag:")
+                    Text(countries[correctAnwser])
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        //flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jacob Tamayo on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
